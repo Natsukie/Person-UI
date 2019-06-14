b316d04e84575ae659a506a42b53bead3d5fb74a
Natsukie <aproditeloveu@gmail.com>
2019-06-14 18:19:42 -0500

    Initial Commit

---
+++ Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by Guangzu on 6/14/19.
+//  Copyright © 2019 Guangzu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
